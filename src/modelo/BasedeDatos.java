232ccb3054b9d926654e10769a6acc79d55de046
camilaEsc4 <cescobard@estudiante.uniajc.edu.co>
2025-11-09 13:29:10 -0500

    implementamos BasedeDatos

---
+++ src/modelo/BasedeDatos.java
@@ -0,0 +1,38 @@
+package modelo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BaseDeDatos {
+    private List<Producto> productos;
+
+    public BaseDeDatos() {
+        this.productos = new ArrayList<>();
+    }
+
+    public void agregarProducto(Producto producto) {
+        productos.add(producto);
+    }
+
+    public Producto buscarProductoSku(String sku) {
+        for (Producto p : productos) {
+            if (p.getSku().equalsIgnoreCase(sku)) {
+                return p;
+            }
+        }
+        return null;
+    }
+
+    public List<Producto> buscarTodos() {
+        return productos;
+    }
+
+    public boolean eliminarProducto(String sku) {
+        Producto encontrado = buscarProductoSku(sku);
+        if (encontrado != null) {
+            productos.remove(encontrado);
+            return true;
+        }
+        return false;
+    }
+}
